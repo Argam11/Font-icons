1c50e5384c5a8b79731395b3f0d20a39e55400d8
Aspram <aspram.markosyan7@gmail.com>
2021-02-04 00:49:23 +0400

    add searchbar and icons name

--- src/stories/Icons/Icons.stories.js
+++ src/stories/Icons/Icons.stories.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "../../assets/icons/font-icons.scss";
 import iconsList from "./icons-data.json";
@@ -9,16 +9,36 @@ export default {
   title: "Example/All icons",
   component: Icon,
   argTypes: {
-    backgroundColor: { control: "color" },
+    color: { control: "color" },
+    size: { control: "number" },
   },
 };
 
-export const Icons = () => {
+export const Icons = ({ color, size }) => {
+  const [icons, setIcons] = useState(iconsList);
+  const [search, setSearch] = useState("");
+
+  const onChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  useEffect(() => {
+    setIcons(iconsList.filter((icon) => icon.toLowerCase().includes(search.toLowerCase())));
+  }, [search]);
+
   return (
     <div className="storybook-icons-container">
-      {iconsList.map((icon, index) => {
-        return <Icon key={index} name={icon} />;
-      })}
+      <input type="text" placeholder="Search..." value={search} onChange={onChange} />
+      <div>
+        {icons.map((icon, index) => {
+          return (
+            <div className="storybook-icon-block" key={index}>
+              <Icon name={icon} color={color} size={size} />
+              <p>{icon}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
